3777703131e619a86a7ed85000bc1e8e5ab1b553
LukeMichaels <lmichaels@gmail.com>
2015-07-10 14:01:14 -0700

    improve activation page styling

--- wp-content/themes/newgameplus/registration/activate.php
+++ wp-content/themes/newgameplus/registration/activate.php
@@ -8,56 +8,69 @@
  */
 ?>
 <?php get_header( 'buddypress' );?>
+<div class="registration-page-wrap">
 
-	<div id="content">
-		<div class="padder">
+  <div class="registration-wrap">
+    <div id="social-container">
+    
+    	<div id="content">
+    		<div class="padder">
 
-		<?php do_action( 'bp_before_activation_page' ) ?>
+      		<?php do_action( 'bp_before_activation_page' ) ?>
+      
+      		<div class="page" id="activate-page">
+      
+      			<?php if ( bp_account_was_activated() ) : ?>
+      
+      				<h2 class="widgettitle"><?php _e( 'Account Activated', 'buddypress' ) ?></h2>
+      
+      				<?php do_action( 'bp_before_activate_content' ) ?>
+      
+      				<?php if ( isset( $_GET['e'] ) ) : ?>
+      					<p><?php _e( 'Your account was activated successfully! Your account details have been sent to you in a separate email.', 'buddypress' ) ?></p>
+      				<?php else : ?>
+      					<p><?php _e( 'Your account was activated successfully! You can now log in with the username and password you provided when you signed up.', 'buddypress' ) ?></p>
+      				<?php endif; ?>
+      
+      			<?php else : ?>
+      
+      				<h3><?php _e( 'Activate your Account', 'buddypress' ) ?></h3>
+      
+      				<?php do_action( 'bp_before_activate_content' ) ?>
+      
+      				<p><?php _e( 'Please provide a valid activation key.', 'buddypress' ) ?></p>
+      
+      				<form action="" method="get" class="standard-form" id="activation-form">
+      
+      					<label for="key"><?php _e( 'Activation Key:', 'buddypress' ) ?></label>
+      					<input type="text" name="key" id="key" value="" />
+      
+      					<p class="submit">
+      						<input type="submit" name="submit" value="<?php _e( 'Activate', 'buddypress' ) ?>" />
+      					</p>
+      
+      				</form>
+      
+      			<?php endif; ?>
+      
+      			<?php do_action( 'bp_after_activate_content' ) ?>
+      
+      		</div><!-- .page -->
+      
+      		<?php do_action( 'bp_after_activation_page' ) ?>
 
-		<div class="page" id="activate-page">
+        </div><!-- .padder -->
+    	</div><!-- #content -->
+    </div><!-- #social-container -->
+  </div><!-- .registration-wrap -->
+  
+  <div class="registration-sidebar">
+    <h2>Login here:</h2>
+    <?php get_sidebar( 'buddypress' ) ?>
+  </div>
+  
+  
+</div><!-- .registration-page-wrap -->
 
-			<?php if ( bp_account_was_activated() ) : ?>
-
-				<h2 class="widgettitle"><?php _e( 'Account Activated', 'buddypress' ) ?></h2>
-
-				<?php do_action( 'bp_before_activate_content' ) ?>
-
-				<?php if ( isset( $_GET['e'] ) ) : ?>
-					<p><?php _e( 'Your account was activated successfully! Your account details have been sent to you in a separate email.', 'buddypress' ) ?></p>
-				<?php else : ?>
-					<p><?php _e( 'Your account was activated successfully! You can now log in with the username and password you provided when you signed up.', 'buddypress' ) ?></p>
-				<?php endif; ?>
-
-			<?php else : ?>
-
-				<h3><?php _e( 'Activate your Account', 'buddypress' ) ?></h3>
-
-				<?php do_action( 'bp_before_activate_content' ) ?>
-
-				<p><?php _e( 'Please provide a valid activation key.', 'buddypress' ) ?></p>
-
-				<form action="" method="get" class="standard-form" id="activation-form">
-
-					<label for="key"><?php _e( 'Activation Key:', 'buddypress' ) ?></label>
-					<input type="text" name="key" id="key" value="" />
-
-					<p class="submit">
-						<input type="submit" name="submit" value="<?php _e( 'Activate', 'buddypress' ) ?>" />
-					</p>
-
-				</form>
-
-			<?php endif; ?>
-
-			<?php do_action( 'bp_after_activate_content' ) ?>
-
-		</div><!-- .page -->
-
-		<?php do_action( 'bp_after_activation_page' ) ?>
-
-		</div><!-- .padder -->
-	</div><!-- #content -->
-
-	<?php get_sidebar( 'buddypress' ) ?>
 
 <?php get_footer( 'buddypress' ); ?>
